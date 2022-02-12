889f6de5ae37b9eeeb2323e05f0ab90847f9e603
Rodrigo Reis <onlinebordados@gmail.com>
2022-02-12 18:01:58 +1300

    alt back php

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php
 
-// $vnome=$_GET["fname"];
-// $vemail=$_GET["femail"];
-// $vtext=$_GET["textarea"];
-// $vdate=$_GET["date"];
-// $vdate2=$_GET["date2"];
-// $vcheck=$_GET["check"];
+$vnome=$_GET["fname"];
+$vemail=$_GET["femail"];
+$vtext=$_GET["textarea"];
+$vdate=$_GET["date"];
+$vdate2=$_GET["date2"];
+$vcheck=$_GET["check"];
 
-echo "Nome: ".$vnome."<br>Email: ".$vemail."<br>Comentario: ".$vtext."<br> Data1: ".$vdate."<br>
-Data2: ".$vdate2."<br>Hamilton: ".$vcheck."<br>";
-
-?>
+echo "Nome: $vnome <br> Email: $vemail <br> Comentario: $vtext <br> Data1: $vdate <br>
+Data2: $vdate2 <br> Hamilton: $vcheck <br>";
